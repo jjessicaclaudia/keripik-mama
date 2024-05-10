6dc1f7d0693d3cb8ae1c478b5e2416f3b06ed71d
jjessicaclaudia <165563475+jjessicaclaudia@users.noreply.github.com>
2024-05-10 16:38:37 +0700

    First Commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+const toggles = document.querySelectorAll('.faq-toggle');
+
+toggles.forEach(toggle => {
+  toggle.addEventListener('click', () => {
+    toggle.parentNode.classList.toggle('active');
+  });
+});
